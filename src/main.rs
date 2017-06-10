3041565ddba06ec2ab309d394c3e358b2dea9835
Jason Willems <jason@willems.ca>
2017-06-10 14:43:34 -0700

    Add CLI args for text, horizontal scale

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,32 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
-}
-
 extern crate image;
 extern crate imageproc;
 extern crate rusttype;
 
-use std::path::Path;
 use imageproc::drawing::{draw_text_mut, draw_filled_rect_mut};
 use imageproc::rect::Rect;
 use image::{Rgb, RgbImage};
 use rusttype::Scale;
+use std::env;
+use std::path::Path;
 
 
 pub fn main() {
-  let icon_text = "FM";
-  create_favicon(icon_text, "./favicon.ico",   16u32);
-  create_favicon(icon_text, "./favicon32.png", 32u32);
-  create_favicon(icon_text, "./favicon48.png", 48u32); 
-  create_favicon(icon_text, "./favicon64.png", 64u32); 
+
+  /* 
+     Usage: 
+      cargo run {favicon text} {horizontal offset as % (positive number)}
+      cargo run FM 30
+   */
+  let icon_text: String = match env::args().nth(1) {
+    Some(icon_text) => icon_text.to_string(),
+    None => "FM".to_string() /* Default FaviconMaker "FM" */
+  };
+
+  create_favicon(&icon_text, "./favicon.ico",    16u32);
+  create_favicon(&icon_text, "./favicon32.png",  32u32);
+  create_favicon(&icon_text, "./favicon48.png",  48u32); 
+  create_favicon(&icon_text, "./favicon64.png",  64u32); 
+  create_favicon(&icon_text, "./favicon512.png", 512u32); 
 }
 
 pub fn create_favicon(txt: &str, filepath: &str, dimensions: u32) {
@@ -37,13 +42,26 @@ pub fn create_favicon(txt: &str, filepath: &str, dimensions: u32) {
                        Rect::at(font_offset as i32, font_offset as i32).of_size(dimensions - font_offset, dimensions - font_offset),
                        black);
 
+  /* Font Parameters */
   let font = include_bytes!("assets/DejaVuSans.ttf") as &[u8];
   let height = dimensions; 
-  let scale = Scale { x: (height as f32) * 0.8, y: (height as f32) * 0.8 };
+  let scale = Scale {
+    x: (height as f32) * 0.8,
+    y: (height as f32) * 0.8
+  };
 
+  let horizontal_offset: u32 = match env::args().nth(2) {
+    Some(h_offset) => {
+      let percent_offset = h_offset.parse::<f32>().unwrap_or(0.0) / 100 as f32;
+      (percent_offset * dimensions as f32) as u32
+    },
+    _ =>
+      0
+  };
   let vertical_offset = dimensions as f32 * 0.1;
 
-  draw_text_mut(&mut image, white, 0, vertical_offset as u32, scale, font, &txt.trim());
+
+  draw_text_mut(&mut image, white, horizontal_offset, vertical_offset as u32, scale, font, &txt.trim());
 
   let _ = image.save(path).unwrap();
 }
